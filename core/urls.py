61a6c5c8870924f05dfaae5ea4ebda8027a8700a
SvetozarP <145108913+SvetozarP@users.noreply.github.com>
2025-05-21 21:50:17 +0100

    Added bar chart

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.chart, name='chart'),
+    path('bar/', views.yearly_avg_co2, name='bar'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Avg
 from django.shortcuts import render
 from core.models import CO2
 import plotly.express as px
@@ -34,3 +35,32 @@ def chart(request):
     }
 
     return render(request, 'core/chart.html', context)
+
+
+def yearly_avg_co2(request):
+    averages = CO2.objects.values('date__year').annotate(avg=Avg('average'))
+    xdata = averages.values_list('date__year', flat=True)
+    ydata = averages.values_list('avg', flat=True)
+
+    text = [f'{avg:.0f}' for avg in ydata]
+
+    fig = px.bar(x=xdata, y=ydata, text=text)
+
+    fig.update_layout(
+        title_text='Average CO2 concentration per Year',
+        yaxis_range=[0, 500],
+    )
+
+    fig.update_traces(
+        textfont_size=16,
+        textangle=-90,
+        textposition='outside',
+        cliponaxis=False,
+    )
+
+    chart_fg = fig.to_html()
+    context = {'chart': chart_fg}
+
+    return render(request, 'core/chart.html', context)
+
+
